d13a7acf01f0ea938847e10b774a38922cadd80b
Romina Steinberg <romina@MacBook-Pro-de-Romina.local>
2019-11-18 21:49:01 -0300

    ingresos

---
+++ backend/models/ingresosModel.js
@@ -0,0 +1,53 @@
+const pool = require('../bd');
+
+async function getIngresos(id) {
+    try {
+        let query = "SELECT id_i, monto_i, categoria_i FROM ?? JOIN ?? ON id_categoria_i = id_ci WHERE id_usuario_i = ?";
+        const rows = await pool.query(query, [process.env.TABLA_INGRESOS, process.env.TABLA_CATEGORIAS_INGRESOS, id]);
+        return rows;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+async function getIngresosPorCat(id, cat) {
+    try {
+        let query = "SELECT id_i, monto_i, categoria_i FROM ?? JOIN ?? ON id_categoria_i = id_ci WHERE id_usuario_i = ? AND categoria_i = ?";
+        const rows = await pool.query(query, [process.env.TABLA_INGRESOS, process.env.TABLA_CATEGORIAS_INGRESOS, id, cat]);
+
+        console.log(rows);
+        return rows;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+async function nuevoIngreso(obj) {
+    try {
+        let query = "INSERT INTO ?? SET ?";
+        const rows = await pool.query(query, [process.env.TABLA_INGRESOS, obj]);
+        return rows.insertId;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+async function updateIngreso(obj, id) {
+  try {
+      let query = "UPDATE ?? SET ? WHERE id_u = ?";
+      const rows = await pool.query(query, [process.env.TABLA_INGRESOS, obj, id]);
+      return rows;
+  } catch (error) {
+      console.log(error);
+      throw error;
+  }
+}
+
+module.exports = {
+    getIngresos,
+    nuevoIngreso,
+    getIngresosPorCat
+}
