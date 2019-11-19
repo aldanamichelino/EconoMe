39bf295618eed8ae20289ad5415bc65748dfca89
aldana.michelino <aldana.michelino@gmail.com>
2019-11-18 22:23:06 -0300

    parcial ahorros y cuenta proyecto

--- backend/models/ahorrosModel.js
+++ backend/models/ahorrosModel.js
@@ -15,7 +15,7 @@ async function insertarAhorro(ahorro){
 
 async function getAhorrosUsuario(id_u){
     try {
-        let query = "select monto_a from ?? where id_u_a = ?";
+        let query = "select sum(monto_a) from ?? where id_u_a = ?";
         const rows = await pool.query(query, [process.env.TABLA_AHORROS, id_u]);
         return rows;
     } catch (error) {
@@ -24,6 +24,45 @@ async function getAhorrosUsuario(id_u){
     }
 }
 
+
+async function getAhorroUsuario(id_u, id_a){
+    try {
+        let query = "select monto_a from ?? where id_u_a = ? and id_a = ?";
+        const rows = await pool.query(query, [process.env.TABLA_AHORROS, id_u, id_a]);
+        return rows;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+
+async function updateAhorro(ahorroModificado, id_u, id_a){
+    try {
+        let query = "update ?? set ? where id_u_a = ? and id_a = ?";
+        const rows = await pool.query(query, ([process.env.TABLA_AHORROS, ahorroModificado, id_u, id_a]));
+        return rows;
+    } catch(error){
+        console.log(error);
+        throw error;
+    }
+}
+
+async function deleteAhorro(id_u, id_a){
+    try {
+        let query = "delete from ?? where id_u_a = ? and id_a = ?";
+        const rows = await pool.query(query, [process.env.TABLA_AHORROS, id_u, id_a]);
+        return rows;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+
 module.exports = {
     insertarAhorro,
-    getAhorrosUsuario }
+    getAhorrosUsuario,
+    getAhorroUsuario,
+    updateAhorro,
+    deleteAhorro }
